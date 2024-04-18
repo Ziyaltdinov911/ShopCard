e87043dbd94d7a0af8f7b9327ea4c7abe7b74716
NSaveMe <_email@example.com>
2024-04-18 15:36:27 +0500

    Initial Commit

---
+++ ShopCard/ShopCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopCardApp.swift
+//  ShopCard
+//
+//  Created by Камиль Байдиев on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
